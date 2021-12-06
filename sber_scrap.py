ef3ab5d54a16e5ac045a143534161d45348c6a75
@ynakimov <akim.off@yandex.ru>
2021-12-06 23:35:18 +0300

    фейк юзер агент

--- sber_scrap.py
+++ sber_scrap.py
@@ -1,6 +1,7 @@
 """
 Скраппинг курсов драгоценных металлог сайта sberbank.ru
 """
+
 from urllib.parse import urlparse
 # from bs4 import BeautifulSoup
 import json
@@ -8,14 +9,11 @@ import json
 # import datetime
 import requests
 # import user_agents as ua
+from fake_useragent import UserAgent
 
 HEADERS_SBER_BANK = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0',
-}
-HEADERS_WIKI = {
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
 }
 
 FULL_URL = 'https://www.sberbank.ru/proxy/services/rates/public/history?rateType=PMR-4&isoCodes[]=A98&date=1638219600000&regionId=038'
@@ -31,11 +29,13 @@ def get_html(full_url, label_redirects=1):
     else:
         allow_redirects = False
 
-    # full_url = 'http://en.wikipedia.org{}'.format(pageUrl)
-    get_result = requests.get(full_url, headers=HEADERS_SBER_BANK, allow_redirects=allow_redirects)
+    ua = UserAgent()
+    headers = HEADERS_SBER_BANK.copy()
+    headers['User-Agent'] = ua.random
+
+    get_result = requests.get(full_url, headers=headers, allow_redirects=allow_redirects)
     result['status'] = get_result.status_code
-    html = get_result.text
-    result['html'] = html
+    result['html'] = get_result.text
     result['json'] = get_result.json()
 
     return result
@@ -47,9 +47,10 @@ def save_as_readable_json(text):
 
 
 get_html = get_html(FULL_URL)
-text = get_html['html']
+text_html = get_html['html']
 text_json = get_html['json'] 
 # print(text)
 print(text_json)
 
 save_as_readable_json(text_json)
+
